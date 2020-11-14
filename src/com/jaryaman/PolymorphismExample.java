8cb200757e829f6c384c7ff545d930ae3c00b1ed
jaryaman <j.aryaman25@gmail.com>
2020-11-14 12:18:20 +0000

    Comment polymorphism example

--- src/com/jaryaman/PolymorphismExample.java
+++ src/com/jaryaman/PolymorphismExample.java
@@ -3,8 +3,14 @@ package com.jaryaman;
 public class PolymorphismExample {
     public static void main(){
         drawUIControl(new TextBox());
+        drawUIControl(new CheckBox());
     }
     public static void drawUIControl(UIControl control){
+        /*
+        * This is the point. At run time, the control object can take on many different forms.
+        * poly = many; morphic = forms. It can behave as a checkbox, a textbox, or any other concrete implementation
+        * of a UI control
+        * */
         control.draw();
     }
 }
