ed9999873686d62869cf34e69bbd000f18171114
jaryaman <j.aryaman25@gmail.com>
2020-11-17 20:47:06 +0000

    Fix docstring

--- src/com/jaryaman/memento/EditorState.java
+++ src/com/jaryaman/memento/EditorState.java
@@ -1,7 +1,7 @@
 package com.jaryaman.memento;
 // The Memento
 public class EditorState {
-    // fClarifinal means, once initialised, it cannot be changed again. It is permanent.
+    // final means, once initialised, it cannot be changed again. It is permanent.
     // Hence we don't need a setter because you only set once, through the constructor
     private final String content;
 
